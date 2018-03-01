1c618d75c1f0456392b07eb38aa1b4a4ff8dbd1b
Stanislau-Tuchto <36663878+Stanislau-Tuchto@users.noreply.github.com>
2018-03-01 20:47:00 +0300

    Update index.js
    Implement getzerofunction

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+    let zeros = 0, i = 1,j=2, five_pow ,primal,flag = false;
+    while(j<=Math.sqrt(base)){
+        if(base%j === 0){
+            base = base/j;
+            flag = true;
+          }else{
+              j++;
+            }
+         if(!flag) j= base;
+      }
+    flag = true;
+    while(flag) {
+        five_pow = Math.pow(j,i)
+        if(five_pow<number){
+            zeros += (number - number %five_pow)/five_pow;
+            i++;
+        }else flag=false;
+    }
+    return zeros
 }
