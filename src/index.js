8e94871b573a732aa75fc5b2cb2a7453667b70a3
Stanislau-Tuchto <36663878+Stanislau-Tuchto@users.noreply.github.com>
2018-03-01 21:20:36 +0300

    Update index.js
    Fix primal

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
-    let zeros = 0, i = 1,j=2, five_pow ,primal,flag = false;
+    let zeros = 0, i = 1,j=2, five_pow ,primal,flag = true;
     while(j<=Math.sqrt(base)){
         if(base%j === 0){
             base = base/j;
-            flag = true;
+           
           }else{
               j++;
             }
-         if(!flag) j= base;
+       
       }
-    flag = true;
+ 
     while(flag) {
-        five_pow = Math.pow(j,i)
+        five_pow = Math.pow(base,i)
         if(five_pow<number){
             zeros += (number - number %five_pow)/five_pow;
             i++;
