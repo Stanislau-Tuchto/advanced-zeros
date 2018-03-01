e2140145f7346d76a574cd95864e76ccbabe170e
Stanislau-Tuchto <toshiro_mifune@mail.ru>
2018-03-01 12:55:39 +0300

    Get count of last zeros in 10 base

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+    let count="";
+    let zeros = 0, i = 1, five_pow ,flag = true;
+    while(flag) {
+        five_pow = Math.pow(5,i)
+        if(five_pow<number){
+            zeros += (number - number %five_pow)/five_pow;
+            i++;
+        }else flag=false;
+    }
+    console.log(zeros+" zeros");
+    // base =8;
+    // zeros = 615;
+    if(10 !== base){
+        while(zeros>=base){
+            count+=zeros%base+";";
+
+            zeros=(zeros - zeros%base)/base;
+            console.log(zeros+" z");
+
+        }
+        count+=zeros;
+        console.log(count.split(';').reverse().join('')+" zb");
+
+    }else return zeros;
+    return count;
 }
